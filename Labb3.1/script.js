70934b51570207b1539607c70c4f61737f1061d3
CashmereBob <weidmar@gmail.com>
2016-12-19 11:36:59 +0100

    bugfix 3.1

--- Labb3.1/script.js
+++ Labb3.1/script.js
@@ -7,9 +7,7 @@ MyApp.users = [
         lastname: "Weidmar",
         telnummer: "0767878060",
         city: "Helsingborg",
-        age: 31,
-        name: function(){return this.firstname + " " + this.lastname
-        }
+        age: 31
     },
     {
         username: "2",
@@ -17,8 +15,7 @@ MyApp.users = [
         lastname: "Nilsson",
         telnummer: "0733XXXX",
         city: "Helsingborg",
-        age: 29,
-        name: function (){return this.firstname + " " + this.lastname}
+        age: 29
     },
     {
         username: "3",
@@ -26,17 +23,19 @@ MyApp.users = [
         lastname: "Falk",
         telnummer: "0767XXXXX",
         city: "Helsingborg",
-        age: 30,
-        name: function (){return this.firstname + " " + this.lastname}
+        age: 30
     }
     ]
+
+for (var i = 0 ; i < MyApp.users.length; i++){
+       MyApp.users[i].name = function (){return this.firstname + " " + this.lastname};
+    }
 
 
 $('#button').on('click', function(e){
     for (var i = 0 ; i < MyApp.users.length; i++){
         $('#textOutput').html($('#textOutput').html() + "<br />" + MyApp.users[i].name());
     }
-
 });
 
 
