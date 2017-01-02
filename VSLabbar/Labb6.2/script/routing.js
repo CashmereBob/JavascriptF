18e39e776df13600c00c666a7021534054e5f08b
Magnus Weidmar <weidmar@gmail.com>
2017-01-02 11:37:11 +0100

    Labb 6.2 done

---
+++ VSLabbar/Labb6.2/script/routing.js
@@ -0,0 +1,23 @@
+﻿var fruitApp = angular.module('fruitApp', ['ngRoute']);
+
+fruitApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../partial/_home.html' 
+        })
+        .when('/Bannana', {
+            templateUrl: '../partial/_bannana.html'
+        })
+    .when('/Pear', {
+        templateUrl: '../partial/_pear.html'
+    })
+    .when('/Apple', {
+        templateUrl: '../partial/_apple.html'
+    })
+    .when('/Pineapple', {
+        templateUrl: '../partial/_pineapple.html'
+    })
+        .when('/Peach', {
+        templateUrl: '../partial/_peach.html'
+    })
+});
