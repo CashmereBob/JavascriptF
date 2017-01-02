97bb9dfe2c0e0532cd5c17015143ae0575583982
Magnus Weidmar <weidmar@gmail.com>
2017-01-02 13:29:15 +0100

    Labb 6.3 done

---
+++ VSLabbar/Labb6.3/scripts/registryAppJS.js
@@ -0,0 +1,60 @@
+﻿registryApp.factory('registryAppFactory', function () {
+
+    var persons = [{}];
+
+    var seminar = {
+        name: '',
+        date: ''
+    };
+
+
+    var factory = {};
+
+    factory.AddPerson = function (person) {
+        presons.push(person);
+    };
+
+    factory.GetAllPersons = function () {
+        return persons;
+    };
+
+    factory.SetSeminar = function (addSeminar) {
+        seminar = addSeminar;
+    };
+
+    factory.GetSeminar = function () {
+        return seminar;
+    };
+
+    return factory;
+});
+
+var controllers = {};
+
+controllers.registryAppController = function ($scope, $location) {
+    $scope.addPersons = function () {
+        registryAppFactory.addSeminar({ name: $scope.seminarName, date: $scope.seminarDate });
+        $location.path('/personRegister');
+    };
+
+    $scope.registerMore = function () {
+        registryAppFactory.addPerson({ name: $scope.personName, phone: $scope.personPhone });
+        $location.path('/personRegister');
+    };
+
+    $scope.registerDone = function () {
+        registryAppFactory.addPerson({ name: $scope.personName, phone: $scope.personPhone });
+        $location.path('/done');
+    };
+
+    $scope.persons = registryAppFactory.GetAllPersons();
+
+    $scope.registeredSeminarName = registryAppFactory.GetSeminar().name;
+    $scope.registeredSeminarDate = registryAppFactory.GetSeminar().date;
+    $scope.seminarName = "hej";
+
+
+
+};
+
+registryApp.controller(controllers);
---
+++ VSLabbar/Labb6.3/scripts/routing.js
@@ -0,0 +1,76 @@
+﻿var registryApp = angular.module('registryApp', ['ngRoute']);
+
+registryApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../partials/_home.html'
+        })
+        .when('/register', {
+            templateUrl: '../partials/_register.html',
+            controller: "registryAppController"
+        })
+        .when('/personRegister', {
+            controller: 'registryAppController',
+            templateUrl: '../partials/_personRegister.html'
+        })
+        .when('/done', {
+            controller: 'registryAppController',
+            templateUrl: '../partials/_done.html'
+        })
+})
+
+registryApp.factory('registryAppFactory', function () {
+
+    var persons = [{}];
+
+    var seminar = {
+        name: '',
+        date: ''
+    };
+
+
+    var factory = {};
+
+    factory.AddPerson = function (person) {
+        persons.push(person);
+    };
+
+    factory.GetAllPersons = function () {
+        return persons;
+    };
+
+    factory.SetSeminar = function (addSeminar) {
+        seminar = addSeminar;
+    };
+
+    factory.GetSeminar = function () {
+        return seminar;
+    };
+
+    return factory;
+});
+
+
+registryApp.controller("registryAppController", function($scope, $location, registryAppFactory) {
+    $scope.AddPersons = function () {
+        registryAppFactory.SetSeminar({ name: $scope.seminarName, date: $scope.seminarDate });
+        $location.path('/personRegister');
+    };
+
+    $scope.registerMore = function () {
+        registryAppFactory.AddPerson({ name: $scope.personName, phone: $scope.personPhone });
+        $scope.personName = null;
+        $scope.personPhone = null;
+    };
+
+    $scope.registerDone = function () {
+        registryAppFactory.AddPerson({ name: $scope.personName, phone: $scope.personPhone });
+        $location.path('/done');
+    };
+
+    $scope.RegisteredPersons = registryAppFactory.GetAllPersons();
+
+    $scope.registeredSeminarName = registryAppFactory.GetSeminar().name;
+    $scope.registededSeminarDate = registryAppFactory.GetSeminar().date;
+
+});
